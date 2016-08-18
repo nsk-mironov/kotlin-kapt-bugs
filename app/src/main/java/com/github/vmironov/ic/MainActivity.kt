45448ecb3f5d4e007f365357518f2b0c4dfad361
Vladimir Mironov <nsk.mironov@gmail.com>
2016-08-18 17:04:10 +0300

    Update dependencies

--- app/src/main/java/com/github/vmironov/ic/MainActivity.kt
+++ app/src/main/java/com/github/vmironov/ic/MainActivity.kt
@@ -1,11 +1,14 @@
 package com.github.vmironov.ic
 
 import android.app.Activity
+import android.databinding.BindingAdapter
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.widget.TextView
+import com.github.vmironov.ic.databinding.ActivityMainBinding
 
 class MainActivity : Activity() {
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_main)
   }
 }
